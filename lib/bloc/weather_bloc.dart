510b4a8150ae13ee4e206145a58cad0ed21aa88d
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2019-06-19 20:44:30 +0300

    handle GetWather event

--- lib/bloc/weather_bloc.dart
+++ lib/bloc/weather_bloc.dart
@@ -1,15 +1,26 @@
 import 'dart:async';
 import 'package:bloc/bloc.dart';
+import 'package:bloc_weather_app/model/weather.dart';
 import './bloc.dart';
 
 class WeatherBloc extends Bloc<WeatherEvent, WeatherState> {
   @override
-  WeatherState get initialState => InitialWeatherState();
+  WeatherState get initialState => WeatherInitial();
 
   @override
   Stream<WeatherState> mapEventToState(
     WeatherEvent event,
   ) async* {
-    // TODO: Add Logic
+    if (event is GetWeather) {
+      yield WeatherLoading();
+      final weather = await _fetchWatherFromFakeApi(event.cityName);
+      yield WeatherLoaded(weather);
+    }
+  }
+
+  Future<Weather> _fetchWatherFromFakeApi(String cityName) {
+    return Future.delayed(Duration(seconds: 2), () {
+      return Weather(cityName: cityName, temperature: 32);
+    });
   }
 }
